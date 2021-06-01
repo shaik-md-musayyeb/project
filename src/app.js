003ea9faf103b3cd951c7e8e53c96fd0f38cc9a8
shaik-md-musayyeb <smd52952@gmail.com>
2021-06-01 14:40:15 +0530

    updated code redability by splitting the js file into two

---
+++ src/app.js
@@ -0,0 +1,133 @@
+import {data} from "./data.js";
+
+var correct = document.getElementById("correct");
+var incorrect = document.getElementById("incorrect");
+// ---function to generate random string---
+var randomWord = (data, level) => {
+  let i = Math.floor(Math.random() * data.length);
+  let j = Math.floor(Math.random() * data[i].length);
+  let item = data[i][j];
+  if (item.length > level || item.length < level)
+    return randomWord(data, level);
+  return item;
+};
+
+var level = 3;
+
+function levelup() {
+  ++level;
+}
+
+var s = 0;
+
+function scoreUp() {
+  ++s;
+}
+
+var count = 0;
+
+function countReset() {
+  count = 0;
+}
+
+function countUp() {
+  ++count;
+}
+
+//function for timer
+var start = 300;
+var time = document.getElementById("timer");
+time.innerHTML = start;
+var interval = setInterval(() => {
+  timer();
+}, 1000);
+
+function timer() {
+  if (start > 0) {
+    start -= 1;
+    time.innerHTML = start;
+    return start;
+  } else {
+    return 0;
+  }
+}
+
+// function to shuffle letters in 'mainWordArray'
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// shiffles the letters of mainWordArray
+// var mainWordArrayShuffled = shuffleArray(mainWordArray);
+
+//converts mainWordArrayShuffled to string
+// var shuffledWord = mainWordArrayShuffled.join("");
+// var length = mainWord.length;
+// var hint1 = mainWord.substr(0, mainWord.length / 2 / 2);
+// var hint2 = mainWord.substr(0, mainWord.length / 2);
+actions();
+
+function actions() {
+  var mainWord = randomWord(data, level);
+  console.log(mainWord);
+  var mainWordArray = mainWord.split("");                   //turns the string into array
+
+  var mainWordArrayShuffled = shuffleArray(mainWordArray);  //shuffles the array (letters of words)
+
+  var shuffledWord = mainWordArrayShuffled.join("");        //array turns into array
+
+  var length = mainWord.length;
+  var hint1 = mainWord.substr(0, mainWord.length / 2 / 2);
+  var hint2 = mainWord.substr(0, mainWord.length / 2);
+  var wordLength = document.getElementById("length");       //finds the length of the word
+  wordLength.innerHTML = length;                            //to set the length in game page
+
+  var question = document.getElementById("question");
+  question.innerHTML = shuffledWord;
+
+  var answer = document.getElementById("answer");
+  answer.setAttribute("maxlength", length);                 //to set the length of user input
+  answer.setAttribute("placeholder", length + " letters");  // to set the value of place holder
+  answer.addEventListener("keyup", validate);               // call the validate method every time the key is up
+  var score = document.getElementById("score");       
+
+  if (answer.value != mainWord && answer.value.length == mainWord.length) {
+    incorrect.play();
+  }
+  // checking answers
+
+  function validate() {
+    let answer = document.getElementById("answer");
+    console.log(answer.value.length);
+    if (answer.value == mainWord) {
+        correct.play();
+        scoreUp();
+        answer.value = "";
+        score.innerHTML = s;
+
+        if (count < 3) countUp();
+        else {
+          levelup();
+          countReset();
+        }
+
+        actions();
+    }
+    // Result
+    if (start == 0) {
+        answer.setAttribute("disabled", "disabled");
+        result.innerHTML = score.innerHTML;
+        document.getElementById("resultDiv").style.display = "block";
+    }
+  }
+}
+// popup menu
+document.getElementById("close").addEventListener("click", close);
+function close() {
+  document.getElementById("resultDiv").style.display = "none";
+}
